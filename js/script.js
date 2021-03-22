fbe9e7e5bc4e90834cb1b60b15398db39cf7fca7
Mirko <mirkosbaglia2001@gmail.com>
2021-03-22 16:00:05 +0100

    Carousel con il controllo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 function prevClick() {
   // console.log('prev');
-  var firstimg = $('img.active')
-  // console.log(firstimg);
-  $('img').addClass('active');
+  var firstImg = $('.active');
+  if (firstImg.hasClass('first')) {
+
+  }
+  else {
+    var prevImg = firstImg.prev('img');
+    firstImg.removeClass('active');
+    prevImg.addClass('active');
+  }
+
 }
 
 
 function nextClick(){
-  // console.log('next');
+  var firstImg = $('.active');   //seleziona qualsiasi elemento che ha  la classe active
+  // console.log(firstImg);
+  if (firstImg.hasClass('last')) {
+
+  }
+  else {
+    var nextImg = firstImg.next('img') // quello che viene subito dopo
+    // console.log(nextImg);
+    firstImg.removeClass('active');
+    nextImg.addClass('active');
+  }
+
+
 }
 
 
